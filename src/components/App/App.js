a6b1d03fa78e4cae99cbf2e847e918ee6ba9325a
wallzack <vulcheek@gmail.com>
2020-01-06 23:09:27 +0100

    12.2: initial commit with webpack items

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className='app-content'>
+        <h1>My first React app</h1>
+        <h2> Hello world! </h2>
+      </main>
+    )
+  }
+}
+
+export default App;
