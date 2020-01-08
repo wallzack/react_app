6a6fc62fe8748f371672dff4e7111dcffa6d7b77
wallzack <vulcheek@gmail.com>
2020-01-08 22:14:03 +0100

    12.4 applied props to class and function component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,8 +4,9 @@ import Hero from '../Hero/Hero.js'
 
 class List extends React.Component {
   render() {
-    return (<section className={styles.component}>
-      <Hero className={styles.subtitle}>Elo elo 3 2 0</Hero> 
+    return (
+      <section className={styles.component}>
+      <Hero titleText={this.props.title} /> 
       </section>
     )
   }
