c85deb5a52af111ca4f51558ab56d9f6e4c34b0c
wallzack <vulcheek@gmail.com>
2020-01-13 17:37:36 +0100

    12.5 added button.js and creator.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/Column.js';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; //wymagana instalacj pakietu prop-types
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser'; //wymagana instalacja pakietu react-html-parser
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
@@ -33,6 +34,9 @@ class List extends React.Component {
           {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
           ))}
+        </div>
+        <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
