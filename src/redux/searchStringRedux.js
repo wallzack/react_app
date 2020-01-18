3e50455a325cf601eac1a3a7228348cd91f746ae
wallzack <vulcheek@gmail.com>
2020-01-18 17:12:48 +0100

    13.5 Task completed.

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -4,14 +4,20 @@ export const countVisibleCards = ({cards, searchString}) => cards.filter(card =>
 export const countAllCards = ({cards}) => cards.length;
 
 // action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createActionChangeSearchString = payload => ({payload, type: CHANGE});
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
