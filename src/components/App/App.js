162e0e784adafb87703f769c1d86fed2b974db3f
wallzack <vulcheek@gmail.com>
2020-01-22 11:25:23 +0100

    14.3 applied changes to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout.js';
 import {AnimatedSwitch} from 'react-router-transition';
 import styles from './App.scss';
+import List from '../List/ListContainer';
+
 
 
 const App = () => (
@@ -20,6 +22,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/FAQ' component={FAQ} />
+        <Route exact path="/list/:id" component={List} />
       </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
